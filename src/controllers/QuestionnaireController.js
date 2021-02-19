bdbda19f6d968e9ff8755b72a76341a8eb89c258
Italo Mota <italomota123@gmail.com>
2021-02-18 23:43:43 -0300

    implementa controller de questionário

---
+++ src/controllers/QuestionnaireController.js
@@ -0,0 +1,46 @@
+const connection = require('../database/connection')
+
+module.exports = {
+  async index (request, response) {
+    const questionnaires = await connection('questionnaires').select(
+      'id',
+      'title'
+    )
+
+    return response.json(questionnaires)
+  },
+  async store (request, response) {
+    const { user } = request.headers
+    const { title, questions } = request.body
+
+    if (!user) {
+      return response.status(400).json({ message: 'User not found!' })
+    }
+
+    const trx = await connection.transaction()
+
+    try {
+      const insertedIds = await trx('questionnaires').insert({
+        title,
+        user
+      })
+
+      const questionnaireId = insertedIds[0]
+
+      const questionsWithId = questions.map(question => ({
+        description: question,
+        questionnaire_id: questionnaireId
+      }))
+
+      await trx('questions').insert(questionsWithId)
+
+      await trx.commit()
+
+      return response.status(201).json()
+    } catch (error) {
+      await trx.rollback()
+
+      return response.status(500).json()
+    }
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,10 @@
 const express = require('express')
 
+const QuestionnaireController = require('./controllers/QuestionnaireController')
+
 const routes = express.Router()
+
+routes.get('/questionnaires', QuestionnaireController.index)
+routes.post('/questionnaires', QuestionnaireController.store)
 
 module.exports = routes
