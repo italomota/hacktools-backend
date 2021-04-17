dc7c3ea0a14525d1ba9a4d4e2e1ea7e3f43a1b36
Ítalo Mota <italo.mota@superlogica.com>
2021-04-17 11:33:19 -0300

    remove campos de latitude e longitude da tabela de respostas

--- src/database/migrations/20210218215040_create_answers.js
+++ src/database/migrations/20210218215040_create_answers.js
@@ -1,8 +1,6 @@
 exports.up = function(knex) {
   return knex.schema.createTable('answers', function(table) {
     table.increments()
-    table.float('latitude').notNullable()
-    table.float('longitude').notNullable()
     table.timestamp('created_at').defaultTo(knex.fn.now())
     table.integer('questionnaire_id').notNullable()
 
