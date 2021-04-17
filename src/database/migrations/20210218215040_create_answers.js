62e60a02f5e5242ca64a272589102ddfcc419a22
Ítalo Mota <italo.mota@superlogica.com>
2021-04-17 12:07:16 -0300

    ajusta ordem das colunas da tabela de respostas

--- src/database/migrations/20210218215040_create_answers.js
+++ src/database/migrations/20210218215040_create_answers.js
@@ -1,8 +1,8 @@
 exports.up = function(knex) {
   return knex.schema.createTable('answers', function(table) {
     table.increments()
-    table.timestamp('created_at').defaultTo(knex.fn.now())
     table.integer('questionnaire_id').notNullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now())
 
     table.foreign('questionnaire_id').references('id').inTable('questionnaires')
   })
